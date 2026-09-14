1d4906989cc06cf2c7e2850b22a89011607b6b58
aubrey <afernandez69@rrc.ca>
2026-09-13 21:23:49 -0500

    adding list of bus stops in busroutes.tsx

--- src/busroutes/busRoutes.ts
+++ src/busroutes/busRoutes.ts
@@ -1,9 +1,9 @@
-export interface BusStop {
+interface BusStop {
     id: number;
     busStopNumber: number;  // 5 digit number eg. #20172
 }
 
-export interface BusRoutes {
+interface BusRoutes {
     id: number;
     routeNumber: string;  // eg BLUE, D14
     routeName: string;  // BLUE St. Norbert, D14 - Ellice
@@ -16,3 +16,23 @@ export interface BusRoutes {
     }
     busNumber?: number;  // Number on the side of the bus
 }
+
+export function BusStops() {
+    const stops: BusStop[] = [
+        {
+            id: 1,
+            busStopNumber: 30154, 
+        },
+
+        {
+            id: 2,
+            busStopNumber: 52173, 
+        },
+
+        {
+            id: 3,
+            busStopNumber: 30154, 
+        },
+    ]
+}
+
