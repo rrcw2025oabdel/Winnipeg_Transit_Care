a973dbfe6a5213cade4e5b12047c48896cac8768
Abdelhamid Oughanem <aoughanem@academic.rrc.ca>
2026-09-14 14:42:48 -0500

    Adding Header.tsx and footer.tsx including the app name in header and team members names in footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer>
+            <p> Team Members </p>
+            <ul>
+                <li>Aubrey Fernandez</li>
+                <li>Abdelhamid Oughanem</li>
+                <li>Keith Robles</li>
+            </ul>        
+        </footer>
+    )
+}
+
+export default Footer;
