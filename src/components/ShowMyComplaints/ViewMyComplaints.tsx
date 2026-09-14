0a9785c87637b60f1bda26b6090f778fa4a080ce
Abdelhamid Oughanem <aoughanem@academic.rrc.ca>
2026-09-14 14:06:29 -0500

    Adding sample Data to be rendred by MyComplaints Function

--- src/components/ShowMyComplaints/ViewMyComplaints.tsx
+++ src/components/ShowMyComplaints/ViewMyComplaints.tsx
@@ -6,3 +6,36 @@ interface Complaint {
   description: string;
   status: string;
 }
+
+function MyComplaints() {
+    const currentUserId = 1;
+    
+    const complaints: Complaint[] =[
+      {
+      complaintId: 1,
+      userId: 1,
+      route: "Route 5 - Portage",
+      title: "Bus arrived late by 30 minutes",
+      description: "The bus arrived approximately 30 minutes late.",
+      status: "Submitted",
+    },
+    {
+      complaintId: 2,
+      userId: 1,
+      route: "Route 15 - Saint Vital",
+      title: "Bus did not stop",
+      description:
+        "The bus passed the stop even though passengers were waiting and did not stop.",
+      status: "In Progress",
+    },
+    {
+      complaintId: 3,
+      userId: 2,
+      route: "Route 10 - North Main",
+      title: "Overcrowded bus",
+      description: "The bus was too crowded to allow more passengers.",
+      status: "Resolved",
+    },
+      
+    ];
+}
