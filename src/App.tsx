27d83436b1a83668ed79311a97db604e7e05c712
Keith <krobles3@rrc.ca>
2026-09-16 08:02:45 -0500

    Added form Complaint and adjusted file naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import CreateComplaint from "./components/CreateComplaint/CreateComplaint";
 import ViewMyComplaints from "./components/ViewMyComplaints/ViewMyComplaints";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
