15b41cd6f4a3537d08e45ed992b99e2feecbf454
Keith <krobles3@rrc.ca>
2026-09-16 07:59:47 -0500

    updated formatting

--- src/components/UserComplantsForm/userComplaintsForm.tsx
+++ src/components/UserComplantsForm/userComplaintsForm.tsx
@@ -8,16 +8,17 @@ interface Complaint {
 }
 
  const availableRoutes: string[] = [
-  "BLUE - Unicity Mall / University of Manitoba",
-  "FX2 - Main / St. Mary's",
-  "FX3 - Regent / Grant",
-  "FX4 - Gateway / Portage",
-  "F5 - McPhillips / Donald",
-  "F6 - McGregor / Osborne",
-  "F7 - St. Anne's / Provencher",
-  "F8 - Henderson / Pembina",
-  "F9 - Lagimodiere / Abinojii Mikanah",
+    "BLUE - Unicity Mall / University of Manitoba",
+    "FX2 - Main / St. Mary's",
+    "FX3 - Regent / Grant",
+    "FX4 - Gateway / Portage",
+    "F5 - McPhillips / Donald",
+    "F6 - McGregor / Osborne",
+    "F7 - St. Anne's / Provencher",
+    "F8 - Henderson / Pembina",
+    "F9 - Lagimodiere / Abinojii Mikanah",
 ];
+
 function CreateComplaint() {
   return (
     <section className="create-complaint"><h2>File a Complaint</h2><form><div><label htmlFor="complaint-route">Bus Route</label><select id="complaint-route" required>            {availableRoutes.map((route) => (
@@ -27,4 +28,5 @@ function CreateComplaint() {
           </label><textareaid="complaint-description"rows={5}
             required/></div><button type="button">Submit Complaint</button></form></section>  );
 }
+
 export default CreateComplaint;
