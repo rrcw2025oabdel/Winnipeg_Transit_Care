bdf267bad7e30de7b37e88e462b45a517460695b
Keith <krobles3@rrc.ca>
2026-09-15 20:20:39 -0500

    Added available bus routes

--- src/components/UserComplantsForm/userComplaintsForm.tsx
+++ src/components/UserComplantsForm/userComplaintsForm.tsx
@@ -6,3 +6,15 @@ interface Complaint {
   description: string;
   status: string;
 }
+
+const availableRoutes: string[] = [
+  "BLUE - Unicity Mall / University of Manitoba",
+  "FX2 - Main / St. Mary's",
+  "FX3 - Regent / Grant",
+  "FX4 - Gateway / Portage",
+  "F5 - McPhillips / Donald",
+  "F6 - McGregor / Osborne",
+  "F7 - St. Anne's / Provencher",
+  "F8 - Henderson / Pembina",
+  "F9 - Lagimodiere / Abinojii Mikanah",
+];
